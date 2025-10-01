f23fe52990671d6499bb3684764699d829e64d34
Baraa Abdullatif <55985500+baraabd@users.noreply.github.com>
2025-10-01 21:34:15 +0100

    Add Computer player class

---
+++ Shotgun_Game/Domain/ComputerPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shotgun_Game.Domain
+{
+    public class ComputerPlayer : Player
+    {
+        public ComputerPlayer(string name) : base(name) { }       
+
+        public override Move ChooseMove(int enemyBullets) => Move.Block;                                       
+    }
+}
