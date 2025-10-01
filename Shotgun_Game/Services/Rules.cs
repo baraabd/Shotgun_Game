cc4fbcbf5bdf518c2cb0e19814e40d5b22c09da6
Baraa Abdullatif <55985500+baraabd@users.noreply.github.com>
2025-10-01 22:16:57 +0100

    Add Rules Class

---
+++ Shotgun_Game/Services/Rules.cs
@@ -0,0 +1,28 @@
+﻿using Shotgun_Game.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shotgun_Game.Services
+{
+    public static class Rules
+    {
+        public static RoundResult Resolve(Move human, Move computer)
+        {
+            RoundResult result = new RoundResult
+            {
+                HumanMove = human,
+                ComputerMove = computer,
+                HumanBulletChange = 0,
+                ComputerBulletChange = 0,
+                GameEnded = false,
+                RoundWinner = RoundWinner.None,
+                Message = "No winner, Lets play again!"
+            };
+            return result;
+        }
+    }
+}
