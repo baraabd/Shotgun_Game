05c48104ab1683033997208cd1cc475798e1781a
Baraa Abdullatif <55985500+baraabd@users.noreply.github.com>
2025-10-01 21:07:51 +0100

    Devine shoot on player

---
+++ Shotgun_Game/Domain/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shotgun_Game.Domain
+{
+    public abstract class Player             
+    {
+        public string Name { get; }           
+        public int Bullets { get; protected set; } 
+
+        protected Player(string name)        
+        {
+            Name = name;                      
+            Bullets = 0;                     
+        }
+
+        public abstract Move ChooseMove(int enemyBullets);
+
+        public bool CanShoot => Bullets > 0;    
+        public bool CanShotgun => Bullets >= 3;   
+    }
+}
